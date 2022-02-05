1a9ddc0b64842abc32379919d2d9fd4c007c7abc
Esteban Beerens <esteban.beerens@gmail.com>
2022-02-05 18:05:29 +0100

    Upgrade custom lang.js

--- public/assets/js/lang.js
+++ public/assets/js/lang.js
@@ -9,23 +9,45 @@ translateHTML();
 /*
  * Language translation logic
 */
-function translateHTML() {
+function translateHTML(language = undefined) {
+    // If the given language is not supported, throw an error
+    if (language && !supportedLanguages.includes(language)) {
+        console.error(`The language "${language}" is not supported.`);
+        return;
+    }
+
     // Get browser language
     const browserLanguage = navigator.language;
 
     // Check if the browser language is supported on the site
     // If the browser language is not supported, use the main lang file
-    const usedLanguage = supportedLanguages.find(l => browserLanguage.includes(l)) ? supportedLanguages.find(l => browserLanguage.includes(l)) : mainLanguage;
+    if (!language) language = supportedLanguages.find(l => browserLanguage.includes(l)) ? supportedLanguages.find(l => browserLanguage.includes(l)) : mainLanguage;
 
     // Get the translation data
-    $.getJSON(`assets/lang/${usedLanguage}.json`, function (translationData) {
+    try {
+        $.getJSON(`assets/lang/${language}.json`, function (translationData) {
 
-        // Set the HTML to have the correct language
-        $('html').attr('lang', usedLanguage);
+            // Set the HTML to have the correct language
+            $('html').attr('lang', language);
 
-        // Populate the HTML fields with the right translation
-        $.each(translationData, function (key, value) {
-            $(`[translation='${key}'`).html(value);
+            // Populate the HTML fields with the right translation
+            $.each(translationData, function (key, value) {
+                // Check if translations are used
+                if ($(`[trans='${key}'`).length) $(`[trans='${key}'`).html(value);
+                else console.warn(`Translation "${key}" was declared in ${language}.json, but is never used.`)
+            });
+
+            // Check if we have missing translations
+            $.each($('[trans]:empty'), function (key, value) {
+                // Mark missing translation in HTML
+                $(value).html(`TRANS_${$(value).attr('trans')}`);
+                $(value).addClass('fw-bold text-danger');
+
+                // Throw error in console
+                console.error(`Missing translation "${$(value).attr('trans')}" in ${language}.json.`);
+            });
         });
-    });
+    } catch (error) {
+        console.error(error);
+    }
 }
